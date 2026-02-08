defe21a62634168eb1e51ad9672e4fa0c3155e94
FAN <bstepan2255@gmail.com>
2026-02-08 17:32:46 +0300

    Update regist.php

--- regist.php
+++ regist.php
@@ -1,1 +1,13 @@
-ппыпвапвапвпв
+<?php
+// regist.php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = isset($_POST['name']) ? trim($_POST['name']) : '';
+
+    // Тут можно сохранить в БД или файл
+    // file_put_contents('registrations.txt', $name . PHP_EOL, FILE_APPEND);
+
+    // Перенаправление обратно на страницу регистрации или на другую страницу
+    header('Location: /registration_success.html'); // если есть страница благодарности
+    exit;
+}
+?>
