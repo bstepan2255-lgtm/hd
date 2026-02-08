5bc54e00b5d900f6fb00f3e4279abf8f4e98a5d6
FAN <bstepan2255@gmail.com>
2026-02-08 17:47:18 +0300

    Update regist.php

--- regist.php
+++ regist.php
@@ -1,11 +1,4 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = isset($_POST['name']) ? trim($_POST['name']) : '';
-
-    // При желании сохранить данные
-
-    // Редирект на страницу благодарности/успеха
-    header('Location: https://bstepan2255-lgtm.github.io/hd/');
-    exit;
-}
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['name'])) {
+    $name = htmlspecialchars(trim($_POST['name']));
 ?>
